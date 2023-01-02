60ab9174d20710188f5dbe090f04072abc77243d
tobiarnold <77453801+tobiarnold@users.noreply.github.com>
2023-01-02 14:10:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def main():
                 x = df.drop(["Name", "überlebt (0=Nein; 1=Ja)"], axis=1)
                 y = df["überlebt (0=Nein; 1=Ja)"]
                 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
-                tf.set_random_seed(42)
+                tf.random.set_seed(42)
                 model = keras.Sequential([
                     keras.layers.Dense(32, activation="relu", input_shape=(x_train.shape[1],)),
                     keras.layers.Dense(16, activation="relu"),
